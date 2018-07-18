02c69a4910768f77fa65e409f1583fc221585889
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2018-07-18 11:44:19 -0700

    Working on cursor

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
 const terminal = document.querySelector("#terminal");
-const cursor = document.querySelector("#cursor")
+const cursor = document.querySelector("#cursor");
 const div = document.querySelector("#empty");
-const resetButton = document.querySelector("#reset-button")
-const whiteButton = document.querySelector("#white-button")
-const blueButton = document.querySelector("#blue-button")
-const greenButton = document.querySelector("#green-button")
+const resetButton = document.querySelector("#reset-button");
+const whiteButton = document.querySelector("#white-button");
+const blueButton = document.querySelector("#blue-button");
+const greenButton = document.querySelector("#green-button");
 
 
 var outputCode =
-  `const terminal = document.querySelector("#terminal");<br>
+  `//Begin script.js<br><br>
+  const terminal = document.querySelector("#terminal");<br>
+  const cursor = document.querySelector("#cursor");<br>
+  const div = document.querySelector("#empty");<br>
+  const resetButton = document.querySelector("#reset-button");<br>
+  const whiteButton = document.querySelector("#white-button");<br>
+  const blueButton = document.querySelector("#blue-button");<br>
+  const greenButton = document.querySelector("#green-button");<br><br>
   var parsedCode = outputCode.split(" ")<br>
-  var i = 0;<br>
+  var i = 0;<br><br>
+
   function addCode() {<br>
-  &emsp;&emsp;terminal.innerHTML += parsedCode[i] + " ";<br><br>
+  &emsp;&emsp;terminal.innerHTML += parsedCode[i] + " ";<br>
+  &emsp;&emsp;div.scrollIntoView();<br><br>
   &emsp;&emsp;if (i == parsedCode.length - 1) {<br>
   &emsp;&emsp;&emsp;&emsp;i = 0;<br>
   &emsp;&emsp;else {<br>
   &emsp;&emsp;&emsp;&emsp;i += 1;<br>
   &emsp;&emsp;}<br>
   }<br><br>
+
+  function reset() {<br>
+  &emsp;terminal.innerHTML = "";<br>
+  &emsp;i = 0;<br>
+  }<br><br>
+
+  function changeColor(color) {<br>
+  &emsp;terminal.style.color = color;<br>
+  }<br><br>
+
+  setInterval(function(){ blinkCursor(); }, 500);<br>
   document.addEventListener("keydown", addCode, false);<br>
+  resetButton.addEventListener("click", reset, false);<br>
+  whiteButton.addEventListener("click", function() { changeColor('white'); } , false);<br>
+  blueButton.addEventListener("click", function() { changeColor('#006AB1'); } , false);<br>
+  greenButton.addEventListener("click", function() { changeColor('#00DE5B'); } , false);<br><br>
+  //End script.js<br><br>
   `
 
 var parsedCode = outputCode.split(" ")
+var bool = true;
 var i = 0;
 
 function addCode() {
@@ -41,7 +67,7 @@ function addCode() {
 }
 
 function blinkCursor() {
-  if (true) {
+  if (bool) {
     cursor.innerHTML = " |";
     bool = false;
   }
@@ -55,17 +81,18 @@ function blinkCursor() {
 function reset() {
   terminal.innerHTML = "";
   i = 0;
-  console.log("Reset Clicked!");
 }
 
 function changeColor(color) {
   terminal.style.color = color;
+  cursor.style.color = color;
 }
 
 // Set interval used for cursor blink
+
 setInterval(function(){ blinkCursor(); }, 500);
 document.addEventListener("keydown", addCode, false);
 resetButton.addEventListener("click", reset, false);
 whiteButton.addEventListener("click", function() { changeColor('white'); } , false);
-blueButton.addEventListener("click", function() { changeColor('#006AB1'); } , false);
-greenButton.addEventListener("click", function() { changeColor('#00DE5B'); } , false);
+blueButton.addEventListener("click", function() { changeColor('#006AB1'); } , false); // Blue color
+greenButton.addEventListener("click", function() { changeColor('#00DE5B'); } , false); // Green color
