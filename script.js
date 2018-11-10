cdac00907ac47d7af256a86f33ff7192d03bdbe0
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2018-11-10 00:43:39 -0800

    refactored javascript logic

--- script.js
+++ script.js
@@ -6,79 +6,78 @@ const whiteButton = document.querySelector("#white-button");
 const blueButton = document.querySelector("#blue-button");
 const greenButton = document.querySelector("#green-button");
 
+var outputCode = [
+  'textfile = input("Enter filename: ")',
+  '<br>',
+  'fh = open(textfile, "r") #read file',
+  '<br>',
+  'fh.seek(0)',
+  '<br>',
+  '#Read from file and save each line to array',
+  '<br>',
+  'lines = []',
+  '<br>',
+  'for line in fh:',
+  '<br>',
+  "&emsp;",
+  "&emsp;",
+  'line_array = line.split("\n")',
+  '<br>',
+  "&emsp;",
+  "&emsp;",
+  'lines.append(line_array[0]) #Add to lines[]',
+  '<br>',
+  'fh.close()'
+];
 
-var outputCode =
-  `//Begin script.js<br><br>
-  const terminal = document.querySelector("#terminal");<br>
-  const cursor = document.querySelector("#cursor");<br>
-  const div = document.querySelector("#empty");<br>
-  const resetButton = document.querySelector("#reset-button");<br>
-  const whiteButton = document.querySelector("#white-button");<br>
-  const blueButton = document.querySelector("#blue-button");<br>
-  const greenButton = document.querySelector("#green-button");<br><br>
-  var parsedCode = outputCode.split(" ")<br>
-  var i = 0;<br><br>
-
-  function addCode() {<br>
-  &emsp;&emsp;terminal.innerHTML += parsedCode[i] + " ";<br>
-  &emsp;&emsp;div.scrollIntoView();<br><br>
-  &emsp;&emsp;if (i == parsedCode.length - 1) {<br>
-  &emsp;&emsp;&emsp;&emsp;i = 0;<br>
-  &emsp;&emsp;else {<br>
-  &emsp;&emsp;&emsp;&emsp;i += 1;<br>
-  &emsp;&emsp;}<br>
-  }<br><br>
-
-  function blinkCursor() {<br>
-  &emsp;if (bool) {<br>
-  &emsp;&emsp;// Change cursor color to same color as the rest of the #terminal<br>
-  &emsp;&emsp;cursor.style.color = cursorColor;<br>
-  &emsp;&emsp;bool = false;<br>
-  &emsp;}<br>
-  &emsp;else {<br>
-  &emsp;&emsp;// Change cursor color to same color as the background<br>
-  &emsp;&emsp;cursor.style.color = 'black';<br>
-  &emsp;&emsp;bool = true;<br>
-  &emsp;}<br>
-  }<br>
-
-  function reset() {<br>
-  &emsp;terminal.innerHTML = "";<br>
-  &emsp;i = 0;<br>
-  }<br><br>
-
-  function changeColor(color) {<br>
-  &emsp;// Keep track of color changes for the cursor as well<br>
-  &emsp;cursorColor = color;<br>
-  &emsp;terminal.style.color = cursorColor;<br>
-  &emsp;cursor.style.color = cursorColor;<br>
-  }<br><br>
-
-  setInterval(function(){ blinkCursor(); }, 500);<br>
-  document.addEventListener("keydown", addCode, false);<br>
-  resetButton.addEventListener("click", reset, false);<br>
-  whiteButton.addEventListener("click", function() { changeColor('white'); } , false);<br>
-  blueButton.addEventListener("click", function() { changeColor('#006AB1'); } , false);<br>
-  greenButton.addEventListener("click", function() { changeColor('#00DE5B'); } , false);<br><br>
-  //End script.js<br><br>
-  `
-
-var parsedCode = outputCode.split(" ")
 var bool = true;
 var cursorColor = ''; // Default is white, this is used for blinkCursor()
-var i = 0;
+var x = 0 // To keep track of lines in outputCode array
+var i = 0; // To keep track of characters
+
+// Get initial line, split by characters
+code = outputCode[x].split("");
 
 function addCode() {
 
-  // Output to <p> tags
-  terminal.innerHTML += parsedCode[i] + " ";
+  terminal.innerHTML += code[i];
 
   // Scroll view automatically to <div> under <p> tag
   div.scrollIntoView();
 
-  if (i == parsedCode.length - 1) {
+  // If last character is reached, reset character iterator to 0
+  if (i == code.length - 1) {
     i = 0;
+
+    // If last line is reached in array, start over
+    if (x == outputCode.length - 1) {
+      terminal.innerHTML += '<br><br>';
+      x = 0;
+      code = outputCode[x].split("");
+    }
+    // If it isn't the last line, go to next line
+    else {
+      x += 1;
+      code = outputCode[x].split("");
+    }
+
+    // If next line is <br> or &emsp; (tab), print immediately
+    if (outputCode[x] == '<br>' || outputCode[x] == '&emsp;') {
+      terminal.innerHTML += outputCode[x];
+      // If last line is reached in array, start over
+      if (x == outputCode.length - 1) {
+        terminal.innerHTML += '<br><br>';
+        x = 0;
+        code = outputCode[x].split("");
+      }
+      // If not, simply go to next line
+      else {
+        x += 1;
+        code = outputCode[x].split("");
+      }
+    }
   }
+  // If last character is not reached, go to next character
   else {
     i += 1;
   }
